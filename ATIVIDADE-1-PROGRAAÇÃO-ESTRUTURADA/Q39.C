b835942fc2efe8d9a91b499a20e6d11e6c1dc70b
Nunno Wakiyama <142551010+nunninono@users.noreply.github.com>
2024-02-29 10:53:58 -0300

    Update Q39.C

---
+++ ATIVIDADE-1-PROGRAAÇÃO-ESTRUTURADA/Q39.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+                            // NAO SEI AINDA REALIZAR A TROCA DE TIPO DE UMA VARIAVEL
+int main(){
+    int num1;
+    printf("Escolha um numero decimal: ");
+    scanf("%d",&num1);
+    if ((num1 % 2) < 0.5){
+        printf("a");
+    }
+  
+}
