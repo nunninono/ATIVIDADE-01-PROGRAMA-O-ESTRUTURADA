3f6a87c139686cc5287bfac9e59f3f931e0e2706
Nunno Wakiyama <142551010+nunninono@users.noreply.github.com>
2024-02-29 10:51:44 -0300

    Update Q31.C

---
+++ ATIVIDADE-1-PROGRAAÇÃO-ESTRUTURADA/Q31.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char email[30];
+    printf("Insira o email: ");
+    scanf("%s",email);
+    printf("Obrigado por cadastrar o e-mail %s",email);
+    return 0;
+}
+
