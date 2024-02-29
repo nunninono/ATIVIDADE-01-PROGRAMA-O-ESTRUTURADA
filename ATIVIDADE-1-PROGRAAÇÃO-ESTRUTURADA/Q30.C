d35ecd49ac94ae40df2979e5e112fc9928d20a36
Nunno Wakiyama <142551010+nunninono@users.noreply.github.com>
2024-02-29 10:51:06 -0300

    Update Q30.C

---
+++ ATIVIDADE-1-PROGRAAÇÃO-ESTRUTURADA/Q30.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num, dobro;
+    printf("Insira um numero: ");
+    scanf("%d",&num);
+    dobro = num * 2;
+    printf("O dobro de %d é %d", num, dobro);
+    return 0;
+}
+
